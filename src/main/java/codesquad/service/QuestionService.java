202cec675d955cc7024a64bbae57551bc981a3d7
ITinnovationHyunkyung <windouble@naver.com>
2018-08-03 00:26:23 +0900

    오타

--- src/main/java/codesquad/service/QuestionService.java
+++ src/main/java/codesquad/service/QuestionService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class QuestionService {
 
     @Autowired
-    QuestionRepository questionRepository;
+//    QuestionRepository questionRepository;
 
     public void setQuestionRepository(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
